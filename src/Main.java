50fa7d73f09f4465fb7316fde26d2267d326a686
arik <19ariknapylov91@gmail.com>
2026-03-17 18:20:45 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Product product1 = new Product(1, "baton", 80.99);
+        System.out.println(product1.getID() + " " + product1.getTitle() + " " + product1.getPrice());
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,29 @@
+public class Product {
+    private int ID;
+    private String title;
+    private double price;
+
+    public Product(int ID, String title, double price){
+        this.ID = ID;
+        this.title = title;
+        this.price = price;
+    }
+    public int getID(){
+        return this.ID;
+    }
+    public void setID(int ID){
+        this.ID = ID;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+}
